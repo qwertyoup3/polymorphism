ee7506a8fbedab783fde2798b6705a0242ca8576
qwertyoup3 <141247977+qwertyoup3@users.noreply.github.com>
2026-04-14 07:06:26 +0700

    menambahkan comment

--- overriding_overloading.java
+++ overriding_overloading.java
@@ -1,3 +1,4 @@
+// Contoh Overriding dan Overloading dalam Java
 public class overriding_overloading {
     public static void main(String[] args) {
         Animal animal = new Animal();
@@ -6,12 +7,14 @@ public class overriding_overloading {
         animal.speak(); // Memanggil method versi kelas induk
         cat.speak();    // Memanggil method yang dioverride di kelas anak
 
-        Math math = new Math();
+        // Membuat objek Math untuk menguji overloading
+        Math math = new Math(); 
         System.out.println("2 + 3 = " + math.add(2, 3));
         System.out.println("2 + 3 + 4 = " + math.add(2, 3, 4));
     }
 }
 
+// Contoh Overriding: Kelas Cat mengoverride method speak() dari kelas Animal
 class Animal {
     void speak() {
         System.out.println("Animal: Suara umum");
@@ -25,6 +28,7 @@ class Cat extends Animal {
     }
 }
 
+// Contoh Overloading: Kelas Math memiliki dua method add() dengan parameter yang berbeda
 class Math {
     int add(int a, int b) {
         return a + b;
